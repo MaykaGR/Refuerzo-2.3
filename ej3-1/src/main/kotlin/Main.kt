63420b19a74557b8e7b853341d5c206d221e6b24
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2021-12-13 02:34:21 +0100

    Add files via upload

---
+++ ej3-1/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+fun area(base: Double, altura: Double): Double{
+    val area = (base*altura)/2
+    return area
+}
+fun main() {
+    val base = 4.00
+    val altura = 5.00
+    println(area(base,altura))
+}
