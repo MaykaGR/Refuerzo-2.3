ac4490047eea4b8091ebdcbd5d31d098a081a33f
Eduardo Fernández Oliver <32598009+revilofe@users.noreply.github.com>
2021-12-14 13:03:44 +0100

    Update Main.kt
    ok

--- ej1-1/src/main/kotlin/Main.kt
+++ ej1-1/src/main/kotlin/Main.kt
@@ -3,5 +3,5 @@ fun main() {
  var nombre: String?
     println ("Escribe tu nombre: ")
     nombre = readLine()
-    println ("Hola $nombre")
+    println ("Hola $nombre") //<-- comprobaria que nombre no es es nulo antes
 }
