0c6c58cb7a11800b7c0999fa7becc0b199093529
Eduardo Fernández Oliver <32598009+revilofe@users.noreply.github.com>
2021-12-14 13:05:08 +0100

    Update Main.kt
    ok

--- ej1-2/src/main/kotlin/Main.kt
+++ ej1-2/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun calcular (horastrabajadas: String, precioporhora: String): Int {
+fun calcular (horastrabajadas: String, precioporhora: String): Int { //<-- No esta mal, pero intentaria separar la lectura de datos y conversion a enteros de lo que es el calculo. Las funciones deben de hacer solo una cosa.
     var horas = horastrabajadas.toInt()
     var precio = precioporhora.toInt()
     var total= horas*precio
