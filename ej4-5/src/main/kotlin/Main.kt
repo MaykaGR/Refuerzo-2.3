c16d137005cfdb9014fe04179805edaffe1af95f
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2022-06-03 18:04:20 +0200

    Add files via upload

--- ej4-5/src/main/kotlin/Main.kt
+++ ej4-5/src/main/kotlin/Main.kt
@@ -1,21 +1,29 @@
-fun serie(numero: Int){
+import kotlin.math.absoluteValue
+
+fun serie(numero: Int): MutableList<Int>{
     var listaPar = mutableListOf(-2)
     var listaImpar = mutableListOf(1)
+    var cantidad = numero/2-2
     if(numero%2==0){
-        for(i in 0..numero/2){
+        for(i in 0..cantidad){
             listaPar.add(listaPar[i]-2)
             listaImpar.add(listaImpar[i]+2)
         }
     }
+    else {
+        for(i in 0..cantidad+1){
+            listaImpar.add(listaImpar[i]+2)
+        }
+        for(i in 0..cantidad){
+            listaPar.add(listaPar[i]-2)
+        }
+    }
     var listafinal: MutableList<Int> = listaPar
-    listafinal = listafinal.addAll(listaImpar)
-
+    listafinal.addAll(listaImpar)
+    listafinal = listafinal.sortedBy {it.absoluteValue}.toMutableList()
+    return listafinal
 }
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println(serie(50))
 }
