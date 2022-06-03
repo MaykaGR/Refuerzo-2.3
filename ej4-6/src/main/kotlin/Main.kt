584ab05c5d1bb6646c2758812fd45f60c2254b72
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2022-06-03 18:16:41 +0200

    Add files via upload

---
+++ ej4-6/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var grande = 50
+    var peque = 1
+
+    while(grande >= 0){
+        print(peque)
+        print(", "+grande+", ")
+        peque +=2
+        grande -=2
+    }
+}
